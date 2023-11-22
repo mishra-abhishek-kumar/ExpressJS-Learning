c347323f5a2dcbd36dba6cbe956f154cdc1d19b1
mishra_abhishek_kumar <mishraabhishek0807@gmail.com>
2023-11-22 21:57:47 +0530

    understanding middleware & Body-Parser

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("Inside 1st middleware");
-    next();
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use((req, res, next) => {
-    console.log("Inside 2nd middleware");
-    res.send('<h1> hello to node js </h1>');
-});
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button>');
+})
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>This is the default page</h1>');
+})
 
 app.listen(4000, () => {
     console.log("Listening on port 4000");
